bd5142bfff0d088024896eef45d55fc6a5e8148c
knd9 <candela.fiume@gmail.com>
2020-10-24 22:46:52 -0300

    .gitignore agregado, correccion de imports

--- db_functions.py
+++ db_functions.py
@@ -1,5 +1,7 @@
 from pony.orm import db_session, select, count
-from db_entities_relations import User #add the entities and relations as we define them in db_entities_relations.py
+#from db_entities_relations import User #add the entities and relations as we define them in db_entities_relations.py
+from typing import Optional
+
 
 #some users functions
 
