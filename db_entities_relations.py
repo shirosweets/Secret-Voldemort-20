98cf5b10610c507005372df3c09411c2de513d9f
Diegoolei <71725436+Diegoolei@users.noreply.github.com>
2020-10-26 15:34:35 -0300

    Feature in34 create entities and relations of lobbies in db (#4)
    db_entities_realtions: agrega dos entidades (Lobby y Player) y sus relaciones

--- db_entities_relations.py
+++ db_entities_relations.py
@@ -1,28 +1,47 @@
 from pony.orm import Database, PrimaryKey, Required, Optional, Set
+from datetime import datetime
 
 db = Database()
 
 # user entity
 class User(db.Entity):
-    id = PrimaryKey(int, auto=True) 
-    email = Required(str, unique=True)
-    username = Required(str, unique=True, max_len=16)
-    password = Required(str, max_len= 32)
-    photo = Optional(str)
-
+    user_id                 = PrimaryKey(int, auto=True) 
+    user_email              = Required(str, unique=True)
+    user_name               = Required(str, unique=True, max_len=16)
+    user_password           = Required(str, max_len= 32)
+    user_photo              = Optional(str)
+    user_create_dt          = datetime
+    user_lobby              = Set('Lobby')        # many to many relation with User-Lobby, we use '' because Player is declarated after this call
+    user_player             = Set('Player')      # one to many relation with User-Player, we use '' because Player is declarated after this call
+    
 # lobby entity
 
-
-# game entity
+class Lobby(db.Entity):
+    lobby_id                = PrimaryKey(int, auto = True)
+    lobby_creator           = Set(User)         # many to many relation with Lobby-User, we use '' because Player is declarated after this call
+    lobby_players           = Set('Player')     # one to many relation with Lobby-Player, we use '' because Player is declarated after this call
+    lobby_name              = Required(str, unique=True)
+    lobby_max_players       = Required(int)
+    lobby_min_players       = Required(int)
+    
+#game entity
 
 
 # player entity
 
+class Player(db.Entity):
+    #player_game             = Required(Game)   # one to many relation with Player-Game
+    player_lobby            = Required(Lobby)   # one to many relation with Player-Game
+    player_id               = Required(User)    # one to many relation with Player-Game
+    player_number           = Required(int, unique = True)
+    player_nick             = Required(str) 
+    player_role             = Required(int)
+    player_is_alive         = Required(bool)
+    player_chat_blocked     = Required(bool)
+    player_director         = Required(bool)
+    player_minister         = Required(bool)
 
-# board entity
-
-
-# log entity
+#log entity
 
 
 # connect the object 'db' with data base
