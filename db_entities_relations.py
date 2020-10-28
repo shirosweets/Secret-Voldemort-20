d33514c87c16c53e55d40d46e436579dc668aad0
shirosweets <60824319+shirosweets@users.noreply.github.com>
2020-10-28 12:50:07 -0300

    Feature in 23 create board model (proclamation) (#9)
    - Added game, player, board, and log models
    - Resolved conflicts @shirosweets

--- db_entities_relations.py
+++ db_entities_relations.py
@@ -6,16 +6,15 @@ db = Database()
 
 # user entity
 class User(db.Entity):
+    user_lobby                   = Set('Lobby')                            # many to many relation with User-Lobby, we use '' because Player is declarated after this call
+    user_player                  = Set('Player')                           # one to many relation with User-Player, we use '' because Player is declarated after this call
+    user_log                     = Optional('Log')                         # one to one relation with User-Log, we use '' because Log is declarated after this call
     user_id                      = PrimaryKey(int, auto=True)              # auto is auto-incremented
     user_email                   = Required(str, unique=True)              # email can't change
     user_name                    = Required(str, unique=True, max_len=16)  # user_name can't change
     user_password                = Required(str, max_len=32)
     user_image                   = Required(str)                           # photo is selected for default = 0 | 1 | 2 | 3
     user_creation_dt             = Required(datetime)
-    user_lobby                   = Set('Lobby')                            # many to many relation with User-Lobby, we use '' because Player is declarated after this call
-    user_player                  = Set('Player')                           # one to many relation with User-Player, we use '' because Player is declarated after this call
-    user_log                     = Optional('Log')                         # one to one relation with User-Log, we use '' because Log is declarated after this call
-
     # For next sprint
     #user_photo                  = Required(int)
     #user_image                  = Optional(str)                           # image is selected from the computer
--- models.py
+++ models.py
@@ -8,24 +8,24 @@ class UserIn(BaseModel):
     userIn_username: str                        # API Request body
     userIn_password: str                        # API Request body
     userIn_photo: Optional[str]
-
+      
     def valid_format_username(self) -> bool:
         return 3 < len(self.userIn_username) < 17
-
+      
     def valid_format_password(self) -> bool:
         return 7 < len(self.userIn_password) < 33
 
-
+      
 class UserOut(BaseModel):
     userOut_email: str                          # API response
     userOut_username: str                       # API response
     userOut_operation_result: str               # for Successful Operation
-
-
+      
+      
 class UserLogIn(BaseModel):
     logIn_email: str                            # API Request body
     logIn_password: str                         # API Request body
-
+      
 
 # lobby models
 class LobbyInput(BaseModel):
@@ -34,17 +34,18 @@ class LobbyInput(BaseModel):
     lobbyIn_max_players: Optional[int]
     lobbyIn_min_players: Optional[int]
 
-
+      
 # game models
-class Game(BaseModel):
+class ViewGame(BaseModel):
+    #game_board
     game_is_started: bool = False               # Depends on Lobby
-    game_next_minister: int                     #
+    game_next_minister: int                     # Def
     game_failed_elections: int = 0              # = 0 <= 3 then reset to 0
     game_step_turn: int = -1                    # = -1 No asigned
     game_last_director: int = -1                # = -1 No asigned
     game_last_minister: int = -1                # = -1 No asigned
-
-
+      
+        
 # player models
 class PlayerIn(BaseModel):
     player_nick: str                            # = userName Depends on User
@@ -61,10 +62,24 @@ class PlayerOut(BaseModel):
     player_minister: bool = False
     player_last_director: int = -1              # = -1 No asigned
     player_last_minister: int = -1              # = -1 No asigned
+ 
 
-
+ class ViewPlayerGame(BaseModel):
+    player_game_id : int                        # Depends on Game
+    player_number: int                          # Defines order
+    player_nick: str                            # = userName Depends on User
+    player_role: int = -1                       # = -1 No asigned
+    player_is_alive: bool = True                # = True
+    player_chat_blocked: bool = False           # = False
+    player_director: bool = False
+    player_minister: bool = False
+    player_last_director: int = -1              # = -1 No asigned
+    player_last_minister: int = -1              # = -1 No asigned
+      
+ 
 # board models
-class ViewActions(BaseModel):                   # class Actions(int, Enum): int or enum
+class ViewActions(BaseModel): # Depends on ViewBoard if the board_is_spell_active= Tru # int or enum
+    # int or enum
     actions_select_director: int = 0
     actions_select_candidate: int = 1
     actions_inializate_proclamation: int = 2
@@ -73,26 +88,24 @@ class ViewActions(BaseModel):                   # class Actions(int, Enum): int 
     actions_discart_card: int = 5
     actions_end_proclamation: int = 6
 
-
+      
 class ViewBoard(BaseModel):
     board_promulged_fenix: int = 0
     board_promulged_death_eater: int = 0
     board_is_spell_active: int = False
+      
+#class ViewSpells(int, Enum): # Depends on ViewBoard if the board_is_spell_active= True
+#    spell_caos = 0
+#    spell_expelliarmus = 1
+#    spell_avada_kedavra = 2
+#    spell_prophecy = 3                         # adivinacion
+#    spell_imperius = 4
+#    spell_crucio = 5
 
-
-class ViewActions(BaseModel):                   # Depends on ViewBoard if the board_is_spell_active= Tru # int or enum
-    actions_select_director: int = 0
-    actions_select_candidate: int = 1
-    actions_inializate_proclamation: int = 2
-    actions_select_card: int = 3
-    actions_send_card: int = 4
-    actions_discart_card: int = 5
-    actions_end_proclamation: int = 6
-
-
+   
 # log models
 class ViewLog(BaseModel):
-    log_won_games_fenix: int = 0        
-    log_won_games_death_eater: int = 0  
-    log_lost_games_fenix: int = 0       
-    log_lost_games_death_eater: int = 0 
+    log_won_games_fenix: int = 0                # = 0
+    log_won_games_death_eater: int = 0          # = 0
+    log_lost_games_fenix: int = 0               # = 0
+    log_lost_games_death_eater: int = 0         # = 0
