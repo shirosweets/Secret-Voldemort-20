2782b4ca6e02789ff0dee0a68e1a7c466ba54ff5
shirosweets <60824319+shirosweets@users.noreply.github.com>
2020-10-28 11:20:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 from fastapi import FastAPI, HTTPException, status
 from fastapi import WebSocket, WebSocketDisconnect
-from models import *
-from db_entities_relations import *
-from db_functions import *
+import models as md
+import db_entities_relations as dbe
+import db_functions as dbf
+from datetime import datetime
+
 
 app = FastAPI()
 
 
-# user endpoints
+# users endpoints
+@app.post("/users/",
+          status_code=status.HTTP_201_CREATED,
+          response_model=md.UserOut
+          )
+async def create_user(new_user: md.UserIn) -> int:
+    if not new_user.valid_format_username():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="can't parse username"
+        )
+    if not new_user.valid_format_password():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="can't parse password"
+        )
+    if dbf.check_email_exists(new_user.userIn_email):
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail="email already registered"
+        )
+    if dbf.check_username_exists(new_user.userIn_username):
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail="username already registered"
+        )
+    else:
+        dbf.insert_user(
+            new_user.userIn_email,
+            new_user.userIn_username,
+            new_user.userIn_password,
+            new_user.userIn_photo)
+        return md.UserOut(
+            userOut_username=new_user.userIn_username, userOut_email=new_user.userIn_email,
+            userOut_operation_result="Succesfully created!")
 
 
 # lobby endpoints
